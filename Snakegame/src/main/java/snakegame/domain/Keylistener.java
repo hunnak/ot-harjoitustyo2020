1573228b2071e73bcb59b3afcf2857aa725d339f
hunnak <joel.hunnakko@helsinki.fi>
2020-04-13 15:53:56 +0300

    tehty erillinen keylistener-luokka  domainiin

---
+++ Snakegame/src/main/java/snakegame/domain/Keylistener.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame.domain;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author joelhunn
+ */
+public class Keylistener extends KeyAdapter {
+    
+    private Snake snake;
+    public Keylistener(Snake dasnake){
+        this.snake=dasnake;
+    }
+    
+    public void keyPressed(KeyEvent event){
+        if (event.getKeyCode() == KeyEvent.VK_UP) {
+            snake.setDirection(Direction.Up);
+        } else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
+            snake.setDirection(Direction.Right);
+        } else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+            snake.setDirection(Direction.Down);
+        } else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+            snake.setDirection(Direction.Left);
+        }
+    }
+    
+}
